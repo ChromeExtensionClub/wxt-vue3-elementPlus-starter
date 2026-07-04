92b8a646974b69e4584feb40eb1f045f3e5beffa
yxb123456cy <2186256471@qq.com>
2026-07-05 00:20:32 +0800

    chore: :hammer: Configure ElementPlus for automatic import

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,22 @@
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'wxt'
-
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-vue'],
+  manifest: {
+
+  },
+  vite: () => ({
+    plugins: [
+      // ...
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  }),
 })
